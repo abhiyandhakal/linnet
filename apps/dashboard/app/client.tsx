2e2279c9dd16056b31ae5909a1d52354cff85765
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2026-01-21 17:52:41 +0545

    fix(dashboard): clean up client.tsx imports

--- apps/dashboard/app/client.tsx
+++ apps/dashboard/app/client.tsx
@@ -1,6 +1,4 @@
 /// <reference types="vinxi/types/client" />
-import { StartClient } from '@tanstack/start'
 import { hydrateStart } from '@tanstack/start'
-import * as ReactDOM from 'react-dom/client'
 
 hydrateStart()
